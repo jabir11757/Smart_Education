5bf48b5b8f81a3e8cbfb3b6d22e5fa9d70c4aefd
jabir11757 <smjalsaba11757@gmail.com>
2022-10-11 13:58:40 +0600

    Components added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import './Home.css'
 
 const Home = () => {
     const quizzes = useLoaderData();
@@ -9,9 +10,21 @@ const Home = () => {
     console.log(quizzes)
     return (
         <div>
-            {
-                quizzes.data.map(quiz => <Quiz key={quiz.id} quiz={quiz} />)
-            }
+            <div>
+                <h3 className='mt-4'>Intro</h3>
+                <h6 className='my-4'>It's a Big platform to increase knowledge.
+                    Anyone can attend here to spread out him/herself.
+                    So, its a big opportunity for all students.
+                    Please, don't be late to join with us.
+                </h6>
+                <button className='btn btn-dark text-white rounded mb-5'>Join Now</button>
+            </div>
+            <div className='home'>
+
+                {
+                    quizzes.data.map(quiz => <Quiz key={quiz.id} quiz={quiz} />)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Quiz.css'
 
-const Quiz = () => {
+
+const Quiz = ({ quiz }) => {
+    const { id, name, logo, total } = quiz;
     return (
+
         <div>
-            <h2>Quiz</h2>
+            <img src={logo} alt='' />
+            <div className='d-flex'>
+                <p>{name}</p>
+                <p>Quizzes: {total}</p>
+                <button btn btn-dark>Let's Start</button>
+            </div>
         </div>
     );
 };
