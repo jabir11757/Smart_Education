3ce11a83e18c4e4212a2765ce7e1ca9f1a05e780
jabir11757 <smjalsaba11757@gmail.com>
2022-10-12 12:34:55 +0600

    Quiz page working

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
 const QuizDetails = ({ topics }) => {
-    const { correctAnswer, id, question, options } = topics.data.questions;
+    const { question, options, correctAnswer } = topics;
 
-    console.log("question")
+    console.log(topics)
+
 
 
     return (
         <div>
-            {/* <h2>{questions.length} </h2> */}
+            <h6>{question}</h6>
+            <div className="row row-cols-2">
+                <div className="col">
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" />
+                    </div>
+                    <p>{options[0]}</p>
+                </div>
+            </div>
+            <div className="row row-cols-2">
+                <div className="col">
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" />
+                    </div>
+                    <p>{options[1]}</p>
+                </div>
+            </div>
+            <div className="row row-cols-2">
+                <div className="col">
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" />
+                    </div>
+                    <p>{options[2]}</p>
+                </div>
+            </div>
+            <div className="row row-cols-2">
+                <div className="col">
+                    <div className="form-check">
+                        <input className="form-check-input" type="radio" />
+                    </div>
+                    <p>{options[3]}</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/QuizTopic/QuizTopic.js
+++ src/components/QuizTopic/QuizTopic.js
@@ -1,54 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizDetails from '../QuizDetails/QuizDetails';
-// import Toast from '../Toast/Toast';
 import './QuizTopic.css'
 
 const QuizTopic = () => {
 
     const quizData = useLoaderData();
-    // console.log(quizData.data.questions[0].options)
-    const { questions } = quizData.data.questions;
 
-
-
+    const { questions } = quizData.data;
 
     return (
         <div className='container'>
 
             <div className='quiz'>
 
-                <h6>{ }</h6>
-                {/* {
+                {
                     questions.map(topics => <QuizDetails key={topics.id} topics={topics} />)
-                } */}
+                }
 
-                <div className="row row-cols-2">
-                    <div className="col">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" />
-                        </div>
-                        <p>ki obostha tomar, din kal kemon jay ?</p>
-                    </div>
-                    <div className="col">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" />
-                        </div>
-                        <p>ki obostha tomar, din kal kemon jay ?</p>
-                    </div>
-                    <div className="col">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" />
-                        </div>
-                        <p>ki obostha tomar, din kal kemon jay ?</p>
-                    </div>
-                    <div className="col">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" />
-                        </div>
-                        <p>ki obostha tomar, din kal kemon jay ?</p>
-                    </div>
-                </div>
             </div>
 
         </div>
